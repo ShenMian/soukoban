ShenMian/soukoban

commit efcd10a6d2b9e2ca15efea81f5fa4dacb1bdc991

refactor: refactor level.rs

src/level.rs
@@ -232,17 +232,16 @@ impl Level {
     /// (excluding empty line within block comment) and filter out groups
     /// without map data.
     fn to_groups(str: &str) -> impl Iterator<Item = &str> + '_ {
-        str.split(['\n', '|']).filter_map({
+        str.split_inclusive(['\n', '|']).filter_map({
             let mut offset = 0;
             let mut len = 0;
             let mut in_block_comment = false;
             let mut has_map_data = false;
             move |line| {
-                len += line.len() + 1;
+                len += line.len();
 
-                let trimmed_end_line = line.trim_end();
-                let trimmed_line = trimmed_end_line.trim_start();
-                if !in_block_comment && (trimmed_line.is_empty() || offset + len == str.len() + 1) {
+                let trimmed_line = line.trim();
+                if !in_block_comment && (trimmed_line.is_empty() || !line.ends_with(['\n', '|'])) {
                     let group = &str[offset..offset + len - 1];
                     offset += len;
                     len = 0;
@@ -266,7 +265,7 @@ impl Level {
                         }
                         return None;
                     }
-                    if has_map_data || !is_xsb_string(trimmed_end_line) {
+                    if has_map_data || !is_xsb_string(trimmed_line) {
                         return None;
                     }
 

commit 84b9dc64350d990b92a86da2c0378e31401a7ddf

fix: fix a bug introduced in commit efcd10a

src/level.rs
@@ -235,16 +235,16 @@ impl Level {
     /// (excluding empty line within block comment) and filter out groups
     /// without map data.
     fn to_groups(str: &str) -> impl Iterator<Item = &str> + '_ {
-        str.split_inclusive(['\n', '|']).filter_map({
+        str.split(['\n', '|']).filter_map({
             let mut offset = 0;
             let mut len = 0;
             let mut in_block_comment = false;
             let mut has_map_data = false;
             move |line| {
-                len += line.len();
+                len += line.len() + 1;
 
                 let trimmed_line = line.trim();
-                if !in_block_comment && (trimmed_line.is_empty() || !line.ends_with(['\n', '|'])) {
+                if !in_block_comment && (trimmed_line.is_empty() || offset + len == str.len() + 1) {
                     let group = &str[offset..offset + len - 1];
                     offset += len;
                     len = 0;

tests/level.rs
@@ -108,12 +108,21 @@ fn metadata() {
 
 #[test]
 fn create_multiple_levels_with_xsb() {
-    let mut string = String::new();
     for entry in fs::read_dir("assets/").unwrap() {
         let path = entry.unwrap().path();
-        string += &(fs::read_to_string(path).unwrap() + "\n\n");
+        let count = path
+            .to_string_lossy()
+            .rsplit_terminator(['_', '.'])
+            .nth(1)
+            .unwrap()
+            .parse()
+            .unwrap();
+        dbg!(&path);
+        assert_eq!(
+            Level::load(&fs::read_to_string(path).unwrap()).count(),
+            count
+        );
     }
-    assert_eq!(Level::load(&string).count(), 3372);
 }
 
 #[test]