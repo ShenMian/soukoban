b8caff1983837436500879f363b5c4569aeb3d83
ShenMian <sms_school@outlook.com>
2024-07-19 22:06:51 +0100

    refactor(benchmarks): update benchmark functions to use Level instead of Map

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -58,9 +58,9 @@ const WORLDCUP2014: &str = r#"
     -------#--######-------
 "#;
 
-// Title: 一箭十万 (102547步)
-// Author: 20603
 pub const PATH: &str = r#"
+    Title: 一箭十万 (102547步)
+    Author: 20603
     ##################################################
     #   #@    #   ##  #   ##  #   ##  #   ##  #   #  #
     # # #     #       #       #       #       #      #
@@ -114,8 +114,8 @@ pub const PATH: &str = r#"
 "#;
 
 fn tunnel_benchmark(c: &mut Criterion) {
-    let map = Map::from_str(PATH).unwrap();
-    let solver = Solver::new(map.clone(), Strategy::Fast);
+    let level = Level::from_str(PATH).unwrap();
+    let solver = Solver::new(level.map().clone(), Strategy::Fast);
     solver.lower_bounds();
     c.bench_function("calculate tunnels", |b| {
         let solver = solver.clone();
@@ -126,35 +126,32 @@ fn tunnel_benchmark(c: &mut Criterion) {
 }
 
 fn solver_benchmark(c: &mut Criterion) {
-    let map = Map::from_str(PATH).unwrap();
-    c.bench_function("solve level 'PATH' (A*)", |b| {
-        b.iter(|| {
-            let solver = black_box(Solver::new(map.clone(), Strategy::Fast));
-            solver.a_star_search().unwrap();
-        })
-    });
-    c.bench_function("solve level 'PATH' (IDA*)", |b| {
-        b.iter(|| {
-            let solver = black_box(Solver::new(map.clone(), Strategy::Fast));
-            solver.ida_star_search().unwrap();
-        })
-    });
+    let mut bench_solve = |level: Level| {
+        c.bench_function(
+            &format!("solve level '{}' using A*", level.metadata()["title"]),
+            |b| {
+                let solver = black_box(Solver::new(level.map().clone(), Strategy::Fast));
+                b.iter(|| {
+                    solver.a_star_search().unwrap();
+                })
+            },
+        );
+        c.bench_function(
+            &format!("solve level '{}' using IDA*", level.metadata()["title"]),
+            |b| {
+                let solver = black_box(Solver::new(level.map().clone(), Strategy::Fast));
+                b.iter(|| {
+                    solver.ida_star_search().unwrap();
+                })
+            },
+        );
+    };
 
-    let map = load_level_from_file("assets/BoxWorld_100.xsb", 3)
-        .map()
-        .clone();
-    c.bench_function("solve level 'BoxWorld #3' (A*)", |b| {
-        b.iter(|| {
-            let solver = black_box(Solver::new(map.clone(), Strategy::Fast));
-            solver.a_star_search().unwrap();
-        })
-    });
-    c.bench_function("solve level 'BoxWorld #3' (IDA*)", |b| {
-        b.iter(|| {
-            let solver = black_box(Solver::new(map.clone(), Strategy::Fast));
-            solver.ida_star_search().unwrap();
-        })
-    });
+    let level = Level::from_str(PATH).unwrap();
+    bench_solve(level);
+
+    let level = load_level_from_file("assets/BoxWorld_100.xsb", 3);
+    bench_solve(level);
 }
 
 fn deadlock_benchmark(c: &mut Criterion) {
@@ -168,9 +165,9 @@ fn deadlock_benchmark(c: &mut Criterion) {
 }
 
 fn path_finding_benchmark(c: &mut Criterion) {
-    let map = Map::from_str(PATH).unwrap();
+    let level = Level::from_str(PATH).unwrap();
     c.bench_function("box move waypoints", |b| {
-        b.iter(|| box_move_waypoints(black_box(&map), Vector2::new(6, 4)))
+        b.iter(|| box_move_waypoints(black_box(level.map()), Vector2::new(6, 4)))
     });
 }
 
