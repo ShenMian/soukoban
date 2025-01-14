47ea9ae429bbea95f0b4602f8a8ae643d5fee18d
ShenMian <sms_school@outlook.com>
2025-01-14 17:50:37 +0000

    test(deadlock): refactor static deadlock calculation tests
    - Improve code readability and maintainability

--- tests/deadlock.rs
+++ tests/deadlock.rs
@@ -1,13 +1,13 @@
-use soukoban::deadlock::*;
+use soukoban::deadlock;
 
 mod utils;
 use utils::*;
 
 #[test]
-fn calculate_dead_positions() {
+fn calculate_static_deadlocks() {
     let map = load_level_from_file("assets/Microban_155.xsb", 3).into();
-    assert_eq!(calculate_static_deadlocks(&map).len(), 9);
+    assert_eq!(deadlock::calculate_static_deadlocks(&map).len(), 9);
 
     let map = load_level_from_file("assets/BoxWorld_100.xsb", 9).into();
-    assert_eq!(calculate_static_deadlocks(&map).len(), 17);
+    assert_eq!(deadlock::calculate_static_deadlocks(&map).len(), 17);
 }
