094822ecbfb5875fbcd57097db6895601a1cd3f6
ben <benhasanemail@gmail.com>
2025-06-14 14:48:49 -0400

    fix

--- src/node.rs
+++ src/node.rs
@@ -52,17 +52,17 @@ impl Node {
                 {
                     continue;
                 }
-                let new_player_position = box_position - &push_direction.into();
-                if !player_reachable_area.contains(&new_player_position) {
+                let new_player_position_check = box_position - &push_direction.into();
+                if !player_reachable_area.contains(&new_player_position_check) {
                     continue;
                 }
-                let mut new_player_position = box_position;
+                let mut new_player_position = *box_position;
 
                 let mut new_pushes = self.pushes + 1;
                 let mut new_moves = self.moves
                     + find_path(
                         self.state.player_position,
-                        *new_player_position,
+                        new_player_position,
                         |position| {
                             !solver.map()[position].intersects(Tiles::Wall)
                                 && (!self.state.box_positions.contains(&position)
@@ -74,15 +74,15 @@ impl Node {
                     - 1;
 
                 // Skip no influence pushes
-                // while solver
-                //     .tunnels()
-                //     .contains(&(new_box_position, push_direction))
-                // {
-                //     new_player_position = &new_box_position;
-                //     new_box_position += push_direction.into();
-                //     new_pushes += 1;
-                //     new_moves += 1;
-                // }
+                while solver
+                    .tunnels()
+                    .contains(&(new_box_position, push_direction))
+                {
+                    new_player_position = new_box_position;
+                    new_box_position += push_direction.into();
+                    new_pushes += 1;
+                    new_moves += 1;
+                }
 
                 let mut new_box_positions = self.state.box_positions.clone();
                 new_box_positions.remove(&box_position);
@@ -102,7 +102,7 @@ impl Node {
 
                 successors.push(Node::new(
                     State {
-                        player_position: *new_player_position,
+                        player_position: new_player_position,
                         box_positions: new_box_positions,
                     },
                     new_pushes,
