2883c01ca5bb87a171b55473e02e7daf18ddbd20
ShenMian <sms_school@outlook.com>
2026-02-22 21:07:34 +0000

    refactor(benchmarks): update level paths in box_move_waypoints and solver benchmarks

--- benches/benches/path_finding.rs
+++ benches/benches/path_finding.rs
@@ -7,9 +7,14 @@ use soukoban::{Level, path_finding};
 use super::utils::*;
 
 fn box_move_waypoints(c: &mut Criterion) {
-    c.bench_function("path_finding::box_move_waypoints", |b| {
-        let level = Level::from_str(PATH).unwrap();
+    c.bench_function("path_finding::box_move_waypoints '一箭十万'", |b| {
+        let level = Level::from_str(PATH_1).unwrap();
         b.iter(|| path_finding::box_move_waypoints(level.map(), Vector2::new(6, 4)))
+    });
+
+    c.bench_function("path_finding::box_move_waypoints 'Microban 3, #101'", |b| {
+        let level = Level::from_str(PATH_2).unwrap();
+        b.iter(|| path_finding::box_move_waypoints(level.map(), Vector2::new(43, 6)))
     });
 }
 
--- benches/benches/solver.rs
+++ benches/benches/solver.rs
@@ -26,7 +26,7 @@ fn a_star_search(c: &mut Criterion) {
         );
     };
 
-    let level = Level::from_str(PATH).unwrap();
+    let level = Level::from_str(PATH_1).unwrap();
     bench_search(level, Strategy::Fast);
 
     let level = load_level_from_file("assets/BoxWorld_100.xsb", 3);
@@ -51,7 +51,7 @@ fn ida_star_search(c: &mut Criterion) {
         );
     };
 
-    let level = Level::from_str(PATH).unwrap();
+    let level = Level::from_str(PATH_1).unwrap();
     bench_search(level, Strategy::Fast);
 
     let level = load_level_from_file("assets/BoxWorld_100.xsb", 3);
@@ -59,7 +59,7 @@ fn ida_star_search(c: &mut Criterion) {
 }
 
 fn tunnels(c: &mut Criterion) {
-    let level = Level::from_str(PATH).unwrap();
+    let level = Level::from_str(PATH_1).unwrap();
     let solver = Solver::new(level.map().clone(), Strategy::Fast);
     solver.lower_bounds();
     c.bench_function("Solver::tunnels", |b| {
