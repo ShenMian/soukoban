aea6db56580c952d847c9db1e8034ed8ec2de4cb
ShenMian <sms_school@outlook.com>
2026-02-22 22:56:05 +0000

    refactor(path_finding): enhance benchmarks for box_move_waypoints

--- benches/benches/path_finding.rs
+++ benches/benches/path_finding.rs
@@ -2,19 +2,39 @@ use std::str::FromStr;
 
 use criterion::{Criterion, criterion_group};
 use nalgebra::Vector2;
-use soukoban::{Level, path_finding};
+use soukoban::{Level, direction::Direction, path_finding};
 
 use super::utils::*;
 
 fn box_move_waypoints(c: &mut Criterion) {
     c.bench_function("path_finding::box_move_waypoints '一箭十万'", |b| {
-        let level = Level::from_str(PATH_1).unwrap();
-        b.iter(|| path_finding::box_move_waypoints(level.map(), Vector2::new(6, 4)))
+        b.iter_batched_ref(
+            || Level::from_str(PATH_1).unwrap(),
+            |level| path_finding::box_move_waypoints(level.map(), Vector2::new(6, 4)),
+            criterion::BatchSize::SmallInput,
+        )
     });
 
     c.bench_function("path_finding::box_move_waypoints 'Microban 3, #101'", |b| {
-        let level = Level::from_str(PATH_2).unwrap();
-        b.iter(|| path_finding::box_move_waypoints(level.map(), Vector2::new(43, 6)))
+        b.iter_batched_ref(
+            || Level::from_str(PATH_2).unwrap(),
+            |level| path_finding::box_move_waypoints(level.map(), Vector2::new(43, 6)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("path_finding::box_move_waypoints 'beemaze'", |b| {
+        b.iter_batched_ref(
+            || {
+                let mut level = Level::from_str(PATH_3).unwrap();
+                level
+                    .execute_batch([Direction::Right, Direction::Right, Direction::Up])
+                    .unwrap();
+                level
+            },
+            |level| path_finding::box_move_waypoints(level.map(), Vector2::new(4, 44)),
+            criterion::BatchSize::SmallInput,
+        )
     });
 }
 
