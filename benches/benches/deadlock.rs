cff887e9c37f6fa420dad2d6840d3ed48dddaa52
ShenMian <sms_school@outlook.com>
2026-03-03 18:38:09 +0000

    fix(deadlock_benchmarks): replace Map with Level

--- benches/benches/deadlock.rs
+++ benches/benches/deadlock.rs
@@ -1,15 +1,15 @@
 use std::str::FromStr;
 
 use criterion::{BatchSize, Criterion, criterion_group};
-use soukoban::{Map, deadlock};
+use soukoban::{Level, deadlock};
 
 use super::utils::*;
 
 fn compute_unused_floors(c: &mut Criterion) {
     c.bench_function("deadlock::compute_useless_floors", |b| {
-        let map = Map::from_str(WORLDCUP2014).unwrap();
+        let level = Level::from_str(WORLDCUP2014).unwrap();
         b.iter_batched(
-            || map.clone(),
+            || level.map().clone(),
             deadlock::compute_useless_floors,
             BatchSize::SmallInput,
         )
@@ -18,8 +18,8 @@ fn compute_unused_floors(c: &mut Criterion) {
 
 fn compute_static_deadlocks(c: &mut Criterion) {
     c.bench_function("deadlock::compute_static_deadlocks", |b| {
-        let map = Map::from_str(WORLDCUP2014).unwrap();
-        b.iter(|| deadlock::compute_static_deadlocks(&map))
+        let level = Level::from_str(WORLDCUP2014).unwrap();
+        b.iter(|| deadlock::compute_static_deadlocks(level.map()))
     });
 }
 
--- benches/benches/map.rs
+++ benches/benches/map.rs
@@ -1,15 +1,15 @@
 use std::str::FromStr;
 
 use criterion::{BatchSize, Criterion, criterion_group};
-use soukoban::Map;
+use soukoban::Level;
 
 use super::utils::*;
 
 fn canonicalize(c: &mut Criterion) {
     c.bench_function("Map::canonicalize", |b| {
-        let map = Map::from_str(WORLDCUP2014).unwrap();
+        let level = Level::from_str(WORLDCUP2014).unwrap();
         b.iter_batched_ref(
-            || map.clone(),
+            || level.map().clone(),
             |map| map.canonicalize(),
             BatchSize::SmallInput,
         )
