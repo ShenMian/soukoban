ffa767ef494b99c2579e138585c930ada135e95a
ShenMian <sms_school@outlook.com>
2026-03-24 16:05:53 +0000

    test(deadlock): add test cases for freeze deadlock detection
    - Add `assets/Deadlock_2.xsb` containing specific layouts to test freeze deadlocks.
    - Implement `freeze_deadlocks` test in `tests/deadlock.rs` to verify deadlock detection logic.
    - Rename `compute_static_deadlocks` test to `static_deadlocks` for consistency.
    - Update imports to include `HashSet`, `Vector2`, and `Map`.

--- tests/deadlock.rs
+++ tests/deadlock.rs
@@ -1,13 +1,37 @@
-use soukoban::deadlock;
+use std::collections::HashSet;
+
+use nalgebra::Vector2;
+use soukoban::{Map, deadlock};
 
 mod utils;
 use utils::*;
 
 #[test]
-fn compute_static_deadlocks() {
+fn static_deadlocks() {
     let map = load_level_from_file("assets/Microban_155.xsb", 3).into();
     assert_eq!(deadlock::compute_static_deadlocks(&map).len(), 9);
 
     let map = load_level_from_file("assets/BoxWorld_100.xsb", 9).into();
     assert_eq!(deadlock::compute_static_deadlocks(&map).len(), 17);
+}
+
+#[test]
+fn freeze_deadlocks() {
+    let mut freeze_1: Map = load_level_from_file("assets/Deadlock_2.xsb", 1).into();
+    freeze_1.set_box_position(Vector2::new(3, 2), Vector2::new(3, 1));
+    assert!(deadlock::is_freeze_deadlock(
+        &freeze_1,
+        Vector2::new(3, 1),
+        freeze_1.box_positions(),
+        &mut HashSet::new(),
+    ));
+
+    let mut freeze_2: Map = load_level_from_file("assets/Deadlock_2.xsb", 2).into();
+    freeze_2.set_box_position(Vector2::new(5, 2), Vector2::new(4, 2));
+    assert!(deadlock::is_freeze_deadlock(
+        &freeze_2,
+        Vector2::new(4, 2),
+        freeze_2.box_positions(),
+        &mut HashSet::new(),
+    ));
 }
