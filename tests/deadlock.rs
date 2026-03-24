c553ee790d2790b7e73e420979db652571fcb35c
ShenMian <sms_school@outlook.com>
2026-03-24 19:02:25 +0000

    test(deadlock): add static deadlock test case
    - Rename `assets/Deadlock_2.xsb` to `assets/Deadlock_3.xsb` and add a new static deadlock layout.
    - Update `static_deadlocks` test to include a check for the new layout.
    - Refactor `freeze_deadlocks` test to use the renamed asset file and generic variable names.
    - Add `#![allow(clippy::op_ref)]` to suppress specific linting warnings in tests.

--- tests/deadlock.rs
+++ tests/deadlock.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::op_ref)]
+
 use std::collections::HashSet;
 
 use nalgebra::Vector2;
@@ -8,6 +10,9 @@ use utils::*;
 
 #[test]
 fn static_deadlocks() {
+    let map: Map = load_level_from_file("assets/Deadlock_3.xsb", 1).into();
+    assert_eq!(deadlock::compute_static_deadlocks(&map).len(), 9);
+
     let map = load_level_from_file("assets/Microban_155.xsb", 3).into();
     assert_eq!(deadlock::compute_static_deadlocks(&map).len(), 9);
 
@@ -17,21 +22,21 @@ fn static_deadlocks() {
 
 #[test]
 fn freeze_deadlocks() {
-    let mut freeze_1: Map = load_level_from_file("assets/Deadlock_2.xsb", 1).into();
-    freeze_1.set_box_position(Vector2::new(3, 2), Vector2::new(3, 1));
+    let mut map: Map = load_level_from_file("assets/Deadlock_3.xsb", 2).into();
+    map.set_box_position(Vector2::new(3, 2), Vector2::new(3, 1));
     assert!(deadlock::is_freeze_deadlock(
-        &freeze_1,
+        &map,
         Vector2::new(3, 1),
-        freeze_1.box_positions(),
+        map.box_positions(),
         &mut HashSet::new(),
     ));
 
-    let mut freeze_2: Map = load_level_from_file("assets/Deadlock_2.xsb", 2).into();
-    freeze_2.set_box_position(Vector2::new(5, 2), Vector2::new(4, 2));
+    let mut map: Map = load_level_from_file("assets/Deadlock_3.xsb", 3).into();
+    map.set_box_position(Vector2::new(5, 2), Vector2::new(4, 2));
     assert!(deadlock::is_freeze_deadlock(
-        &freeze_2,
+        &map,
         Vector2::new(4, 2),
-        freeze_2.box_positions(),
+        map.box_positions(),
         &mut HashSet::new(),
     ));
 }
